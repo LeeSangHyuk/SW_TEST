86df550e326582f6ebd15b0420f92157109cec14
이상혁 <lshprid@gmail.com>
2021-08-07 06:36:52 +0000

    BOJ_1912_Continuous Sum

--- do.cpp
+++ do.cpp
@@ -1,97 +1,39 @@
-/*벽 부수고 이동하기2*/
+/*BOJ 1912번 연속합*/
 /*
-- 걸린시간 1시간 15분(1시간 정도에 풀었지만, 계속된 오답에 반례를 확인하고 풀었다...
-- 오답 이유 : 벽 부수는 횟수를 큐에 넣고 돌리려 했지만, 반례로 visit 함수의 겹침이 생기는 경우가 있었음
-- 3차원 배열사용 필요
+DP[N] = Max(DP[N - 1] + Arr[i] , Arr[i])
+- 2차 배열 및 2중 for문을 사용하니 메모리 초과 및 시간 초과가 뜸
+- 점화식을 찾아내는 능력 필요
 */
+
 #include <iostream>
-#include <cstdio>
 #include <vector>
-#include <queue>
+#include <algorithm>
 
-#define INF 9999999
 using namespace std;
-
 int N;
-int M;
-int K;
-int Min = INF;
-int dir_x[4] = {0,-1,0,1};
-int dir_y[4] = {-1,0,1,0};
-
-vector<vector<int>> map;
-vector<vector<vector<bool>>> visit;
-
-typedef struct _point{
-	int x;
-	int y;
-	int move_cnt;
-	int break_cnt;
-
-}Point;
+vector<int> dp;
+vector<int> map;
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
-	cin>>N;
-	cin>>M;
-	cin>>K;
-
-	map.resize(N+1,vector<int>(M+1,-1));
-	visit.resize(K+1,vector<vector<bool>>(N+1,vector<bool>(M+1,true)));
-	queue<Point> q;
+	cin >> N;
+	map.resize(N+1,0);
+	dp.resize(N+1,0);
 	
 	for(int i=1;i<=N;i++){
-		for(int j=1;j<=M;j++){
-			scanf("%1d",&map[i][j]);
-		}
+		cin>>map[i];
 	}
-	visit[1][1][1] = false;
-	q.push({1,1,1,K});
 	
-	while(!q.empty()){
-		Point buff = q.front();
-		q.pop();
+	dp[1] = map[1];
+	int answer = dp[1];
 		
-		if(buff.x==N && buff.y==M){
-			if(Min>buff.move_cnt){
-				Min = buff.move_cnt;
-			}
-		}
-		for(int i=0;i<4;i++){
-			int new_x = buff.x + dir_x[i];
-			int new_y = buff.y + dir_y[i];
-			
-			if(new_x>=1 && new_x<=N && new_y>=1 && new_y<=M){
-				if(visit[buff.break_cnt][new_x][new_y]){
-					
-					if(map[new_x][new_y]==0){
-						q.push({new_x,new_y,buff.move_cnt+1,buff.break_cnt});
-						visit[buff.break_cnt][new_x][new_y]= false;
-					}
-					else{
-						if(buff.break_cnt!=0){
-							q.push({new_x,new_y,buff.move_cnt+1,buff.break_cnt-1});
-							visit[buff.break_cnt-1][new_x][new_y]= false;
-						}
-						
-					}
-				}
-				else{
-					continue;
-				}
-			}
-		}
-		
-	}
-	if(Min == INF){
-		cout<< -1<<endl;
-	}
-	else{
-		cout<< Min<<endl;
+	for(int i=2;i<=N;i++){
+		dp[i] = max(map[i],dp[i-1]+map[i]);
+		answer = max(answer,dp[i]);
 	}
 	
-	
+	cout<<answer;
 
     return 0;
 }
