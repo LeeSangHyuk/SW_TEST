17132e1c4704b735ddb9ff36fa274b0e3da6de79
이상혁 <lshprid@gmail.com>
2021-08-24 12:06:45 +0000

    BOJ_11399_ATM(Greedy)

--- do.cpp
+++ do.cpp
@@ -1,94 +1,36 @@
-/*알고리즘 공부 BOJ 17942
-- 1시간 반 정도 풀다가 답지 봄
-- 다익스트라의 응용
-- 공부량이 적은 알고리즘 부터 ㄱㄱ
-- auto 의 사용법 확인
-- priority_queue 의 사용법 다시 확인
+/*알고리즘 공부 
+<그리디알고리즘>
+BOJ 11399 ATM
 */
 
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int N;
-int M;
-int R;
-
-vector<int> study_size;
-
+vector<int> P;
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
-	cin >>N;
-	cin>> M;
-	study_size.resize(N+1);
-	
-	for(int i=1;i<=N;i++){
-		cin>>study_size[i];
+	cin>>N;
+	P.resize(N,0);
+	for(int i=0;i<N;i++){
+		cin>>P[i];
 	}
 	
-	cin>>R;
-	
-	vector<vector<pair<int,int>>> map(N+1);
-	
-	for(int i=0;i<R;i++){
-		int from,to,sub_size;
-		cin>>from;
-		cin>>to;
-		cin>>sub_size;
-		map[from].push_back({to,sub_size});
-		
-	}
-	
-	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-	
-	vector<bool> visit(N+1,false);
-	
-	for(int i=1;i<=N;i++){
-		pq.push({study_size[i],i});
-	}
-	
-	
-	int ans = -1, cnt =0;
+	sort(P.begin(),P.end());
 
-	while(!pq.empty() && cnt<M){
-		pair<int,int> buff = pq.top();
-		
-		int cost = buff.first;
-		int node = buff.second;
-		
-		pq.pop();
-		if(visit[node]) continue;
-		cnt++;
-		visit[node] =true;
-		
-		if(ans<cost){
-			ans = cost;
-		}
-		
-		for(int j =0;j<map[node].size();j++){
-			int next_node = map[node][j].first;
-			int next_cost = map[node][j].second;
-		
-			
-			if(visit[next_node]==true) continue;
-			if(study_size[next_node] >= next_cost){
-				study_size[next_node]=study_size[next_node]-next_cost;
-			}
-			else{
-				study_size[next_node]= 0;
-			}
-			pq.push({study_size[next_node],next_node});
-
-		}
-		
+	int buff = 0;
+	int total = 0;
+	for(int i=0;i<P.size();i++){
+		buff= P[i]+buff;
+		total = buff+total;
 	}
+
 	
-	cout<< ans<<endl;
-	
-	
+	cout<<total;
     return 0;
 }
